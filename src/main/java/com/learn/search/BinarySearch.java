0ab64366c39525cb61081254d8cb1aa79d99f850
rynxiao <yuzhongzi91@sina.com>
2019-09-09 15:00:41 +0800

    feat - implementation of deformation binary search

--- src/main/java/com/learn/search/BinarySearch.java
+++ src/main/java/com/learn/search/BinarySearch.java
@@ -37,4 +37,88 @@ public class BinarySearch {
       return bsearchInternally(arr, 0, high - 1, value);
     }
   }
+
+  public int getFirst(int[] arr, int n, int value) {
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+      int mid = low + ((high - low) >> 1);
+      if (arr[mid] < value) {
+        low = mid + 1;
+      } else if (arr[mid] > value) {
+        high = mid - 1;
+      } else {
+        if ((mid == 0) || (arr[mid - 1] != value)) {
+          return mid;
+        } else {
+          high = mid - 1;
+        }
+      }
+    }
+
+    return -1;
+  }
+
+  public int getLast(int[] arr, int n, int value) {
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+      int mid = low + ((high - low) >> 1);
+      if (arr[mid] < value) {
+        low = mid + 1;
+      } else if (arr[mid] > value) {
+        high = mid - 1;
+      } else {
+        if ((mid == n - 1) || (arr[mid + 1] != value)) {
+          return mid;
+        } else {
+          low = mid + 1;
+        }
+      }
+    }
+
+    return -1;
+  }
+
+  public int getFirstGreatThan(int[] arr, int n, int value) {
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+      int mid = low + ((high - low) >> 1);
+      if (arr[mid] >= value) {
+        if ((mid == 0) || (arr[mid - 1] < value)) {
+          return mid;
+        } else {
+          high = mid - 1;
+        }
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return -1;
+  }
+
+  public int getLastLessThan(int[] arr, int n, int value) {
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+      int mid = low + ((high - low) >> 1);
+      if (arr[mid] <= value) {
+        if ((mid == n - 1) || (arr[mid + 1] != value)) {
+          return mid;
+        } else {
+          low = mid + 1;
+        }
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    return -1;
+  }
 }
--- src/test/java/com/learn/search/BinarySearchTest.java
+++ src/test/java/com/learn/search/BinarySearchTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BinarySearchTest {
   private int[] list;
+  private int[] repeatList;
   private BinarySearch binarySearch;
 
   @BeforeEach
   void setUp() {
     list = new int[] {12, 23, 34, 36, 78, 89, 90, 95, 98, 101, 121};
+    repeatList = new int[] {1, 3, 4, 5, 6, 8, 8, 8, 11, 18};
     binarySearch = new BinarySearch();
   }
 
@@ -33,5 +35,25 @@ class BinarySearchTest {
   @Test
   void should_search_index_failed_given_a_sorted_list_and_a_unexpected_number_use_internally_method() {
     assertEquals(binarySearch.search2(list, 11, 77), -1);
+  }
+
+  @Test
+  void should_get_first_index_successfully_given_a_sorted_list_and_a_number() {
+    assertEquals(binarySearch.getFirst(repeatList, 10, 8), 5);
+  }
+
+  @Test
+  void should_get_last_index_successfully_given_a_sorted_list_and_a_number() {
+    assertEquals(binarySearch.getLast(repeatList, 10, 8), 7);
+  }
+
+  @Test
+  void should_get_first_great_than_index_successfully_given_a_sorted_list_and_a_number() {
+    assertEquals(binarySearch.getFirstGreatThan(repeatList, 10, 5), 3);
+  }
+
+  @Test
+  void should_get_last_less_than_index_successfully_given_a_sorted_list_and_a_number() {
+    assertEquals(binarySearch.getLastLessThan(repeatList, 10, 7), 4);
   }
 }
