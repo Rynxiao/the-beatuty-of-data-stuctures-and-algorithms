310ee44148212f19278d9b9c1c81a525266f614f
rynxiao <yuzhongzi91@sina.com>
2019-10-10 10:18:13 +0800

    feat - implementation of deleting binary search

---
+++ src/test/java/com/learn/tree/BinarySearchTreeTest.java
@@ -0,0 +1,48 @@
+package com.learn.tree;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTreeTest {
+  private BinarySearchTree binarySearchTree;
+
+  @BeforeEach
+  void setUp() {
+    binarySearchTree = new BinarySearchTree();
+    binarySearchTree.insert(33);
+    binarySearchTree.insert(16);
+    binarySearchTree.insert(50);
+    binarySearchTree.insert(13);
+    binarySearchTree.insert(18);
+    binarySearchTree.insert(34);
+    binarySearchTree.insert(58);
+    binarySearchTree.insert(15);
+    binarySearchTree.insert(17);
+    binarySearchTree.insert(25);
+    binarySearchTree.insert(51);
+    binarySearchTree.insert(66);
+    binarySearchTree.insert(19);
+    binarySearchTree.insert(27);
+    binarySearchTree.insert(55);
+  }
+
+  @Test
+  void should_find_data_given_a_binary_search_tree() {
+    assertEquals(27, binarySearchTree.find(27).getData());
+  }
+
+  @Test
+  void should_insert_data_successfully_given_a_binary_search_tree() {
+    binarySearchTree.insert(99);
+    assertNotNull(binarySearchTree.find(99));
+    assertEquals(99, binarySearchTree.find(99).getData());
+  }
+
+  @Test
+  void should_delete_data_successfully_given_a_binary_search_tree() {
+    binarySearchTree.delete(18);
+    assertNull(binarySearchTree.find(18));
+  }
+}
